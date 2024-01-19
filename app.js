17241a18eed7728bd34f3013b4df98832b1d32fb
enrihg <99612570+enrihg@users.noreply.github.com>
2024-01-19 15:56:21 -0300

    random quote feature added

--- app.js
+++ app.js
@@ -1,10 +1,37 @@
 const btnLessMore = document.querySelector('.btn-lessMore');
+const btnRefresh = document.querySelector('.btn-refresh');
 const quote = document.querySelector('.quote');
+const quoteParagraph = document.querySelector('.quote-paragraph');
+const quoteAuthor = document.querySelector('.quote-author');
 const modal = document.querySelector('.modal');
 
+
+//Shows/hide modal and quote section when clicking the less/more button
 btnLessMore.addEventListener('click', () => {
     quote.classList.toggle('display-none');
     modal.classList.toggle('display-none');
-    modal.classList.contains('display-none') ? btnLessMore.firstElementChild.innerText ='MORE' : btnLessMore.firstElementChild.innerText ='LESS';
+    modal.classList.contains('display-none') ? btnLessMore.firstElementChild.innerText = 'MORE' : btnLessMore.firstElementChild.innerText = 'LESS';
 })
 
+
+//Gives a random quote when clicking the refresh button
+btnRefresh.addEventListener('click', randomQuote);
+
+function randomQuote() {
+    fetch('https://api.quotable.io/quotes/random')
+    .then((res) => {
+        console.log('RESOLVED', res);
+        return res.json();
+    })
+    .then((data) => {
+        console.log(data);
+        quoteParagraph.innerText = data[0].content;
+        quoteAuthor.innerText = data[0].author;
+    })
+    .catch((e) => {
+        console.log('ERROR!!', e);
+    });
+    
+}
+
+randomQuote();
