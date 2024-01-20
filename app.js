f63f1ed53daa4be0756e201fbadc8718476ed021
enrihg <99612570+enrihg@users.noreply.github.com>
2024-01-20 20:47:29 -0300

    modal card dinamic data added

--- app.js
+++ app.js
@@ -19,19 +19,46 @@ btnRefresh.addEventListener('click', randomQuote);
 
 function randomQuote() {
     fetch('https://api.quotable.io/quotes/random')
-    .then((res) => {
-        console.log('RESOLVED', res);
-        return res.json();
-    })
-    .then((data) => {
-        console.log(data);
-        quoteParagraph.innerText = data[0].content;
-        quoteAuthor.innerText = data[0].author;
-    })
-    .catch((e) => {
-        console.log('ERROR!!', e);
-    });
-    
+        .then((res) => {
+            console.log('RESOLVED', res);
+            return res.json();
+        })
+        .then((data) => {
+            console.log(data);
+            quoteParagraph.innerText = `"${data[0].content}"`;
+            quoteAuthor.innerText = data[0].author;
+        })
+        .catch((e) => {
+            console.log('ERROR!!', e);
+        });
+
 }
 
 randomQuote();
+
+const weekNumber = document.querySelector('.week-number');
+const weekDay = document.querySelector('.week-day');
+const yearDay = document.querySelector('.year-day');
+const timezone = document.querySelector('.timezone');
+
+//Geolocalization data
+function geolocalization() {
+    fetch('http://worldtimeapi.org/api/ip')
+        .then((res) => {
+            console.log('GEOLOCALIZATION RESOLVED',res);
+            return res.json();
+        })
+        .then((data) => {
+            console.log(data);
+            weekNumber.innerText = data.week_number;
+            weekDay.innerText = data.day_of_week;
+            yearDay.innerText = data.day_of_year;
+            timezone.innerText = data.timezone;
+        })
+        .catch((e) => {
+            console.log('GEOLOCALIZATION ERROR', e);
+        })
+
+}
+
+geolocalization();
