4e315f67b7dc108dae544f520c5e1f3cff316a4c
enrihg <99612570+enrihg@users.noreply.github.com>
2024-01-24 19:00:31 -0300

    clock functionality refactored

--- app.js
+++ app.js
@@ -4,9 +4,36 @@ const quote = document.querySelector('.quote');
 const quoteParagraph = document.querySelector('.quote-paragraph');
 const quoteAuthor = document.querySelector('.quote-author');
 const modal = document.querySelector('.modal');
+const main = document.querySelector('main');
+const showWeekNumber = document.querySelector('.week-number');
+const showWeekDay = document.querySelector('.week-day');
+const showYearDay = document.querySelector('.year-day');
+const showTimezone = document.querySelector('.timezone');
+const showCityAndCountry = document.querySelector('.city-country');
+const showTime = document.querySelector('.hour');
+const showGreeting = document.querySelector('.greeting');
+const showDaytimeIcon = document.querySelector('.daytime-icon');
+const showUtc = document.querySelector('.utc');
+
+let ip= 0;
+let hour= 0;
+let hours= 0;
+let minutes= 0;
+let weekNumber= 0;
+let weekDay= 0;
+let yearDay= 0;
+let unixtime=0 ;
+let timezone= 0;
+let utcOffset='';
+let city='';
+let country='';
+
+getGeolocalizationData();
+updateHour();
+randomQuote();
 
 
-//Shows/hide modal and quote section when clicking the less/more button
+//Shows/hide the modal and quote section when clicking the less/more button
 btnLessMore.addEventListener('click', () => {
     quote.classList.toggle('display-none');
     modal.classList.toggle('display-none');
@@ -20,125 +47,99 @@ btnRefresh.addEventListener('click', randomQuote);
 function randomQuote() {
     fetch('https://api.quotable.io/quotes/random')
         .then((res) => {
-            console.log('RESOLVED', res);
             return res.json();
         })
         .then((data) => {
-            console.log(data);
             quoteParagraph.innerText = `"${data[0].content}"`;
             quoteAuthor.innerText = data[0].author;
         })
         .catch((e) => {
             console.log('ERROR!!', e);
         });
-
 }
 
-randomQuote();
 
-const weekNumber = document.querySelector('.week-number');
-const weekDay = document.querySelector('.week-day');
-const yearDay = document.querySelector('.year-day');
-const timezone = document.querySelector('.timezone');
-
-let ip;
-
-//Geolocalization data
-function geolocalization() {
+//updates the geolocalization object properties values
+function getGeolocalizationData() {
     fetch('http://worldtimeapi.org/api/ip')
         .then((res) => {
-            console.log('GEOLOCALIZATION RESOLVED', res);
             return res.json();
         })
         .then((data) => {
             console.log(data);
-            weekNumber.innerText = data.week_number;
-            weekDay.innerText = data.day_of_week;
-            yearDay.innerText = data.day_of_year;
-            timezone.innerText = data.timezone;
-            ip = data.client_ip;
-            setHour(data.datetime);
-
+            weekNumber = data.week_number; //ver si cambiarlo
+            weekDay = data.day_of_week; //ver si cambiarlo
+            yearDay = data.day_of_year; //ver si cambiarlo
+            timezone = data.timezone;
+            ip = (data.client_ip);
+            utcOffset = data.utc_offset; //ver si cambiarlo
+            unixtime = data.unixtime;
         })
         .then(() => {
-            setCityAndCountry();
+            fetch(`https://ipapi.co/${ip}/json`)
+                .then((res) => {
+                    return res.json();
+                })
+                .then((data) => {
+                    city = data.city;
+                    country = data.country_name;
+                })
+                .catch((e) => {
+                    console.log('IP ERROR!!', e);
+                })
         })
         .catch((e) => {
             console.log('GEOLOCALIZATION ERROR', e);
         })
-
 }
 
-geolocalization();
-setInterval(geolocalization, 60000);
-
-const cityAndCountry = document.querySelector('.city-country');
-
-//Sets the city location and country
-function setCityAndCountry() {
-    fetch(`https://ipapi.co/${ip}/json`)
-        .then((res) => {
-            console.log('IP RESPONSE OK');
-            return res.json();
-        })
-        .then((data) => {
-            console.log(data);
-            cityAndCountry.innerText = `IN ${data.city}, ${data.country_name}`.toUpperCase();
-        })
-        .catch((e) => {
-            console.log('IP ERROR!!', e);
-        })
+//updates the hour every 1 second ???
+function updateHour() {
+    setInterval(() => {
+        console.log(unixtime += 1);;
+        let date = new Date(unixtime * 1000); //ver cada cuanto conviene actualizar los datos
+        hours = date.getHours();
+        minutes = date.getMinutes();
+        hour = Number(`${hours}.${minutes}`);
+        setGeolocalizationData();
+    }, 1000)
 }
 
 
-//Sets the hour
-const time = document.querySelector('.hour');
-
-function setHour(dateTime) {
-    console.log(dateTime);
-    let date = new Date(dateTime);
-    console.log(date);
-    const [hour, minutes] = [date.getHours(), date.getMinutes()];
-    console.log(hour, minutes);
-    time.innerText = `${hour}:${minutes}`;
+//Updates the UI with geolocalization data;
+function setGeolocalizationData() {
+    showWeekNumber.innerText = weekNumber;
+    showWeekDay.innerText = weekDay;
+    showYearDay.innerText = yearDay;
+    showTimezone.innerText = timezone;
+    showCityAndCountry.innerText = `IN ${city}, ${country}`.toUpperCase();
+    showUtc.innerText = `UTC: ${utcOffset}`;
+    showTime.innerText = `${hours}:${minutes}`;
+    greeter()
+    isDayOrNight();
 }
-
-
-
 
 
 //Sets the greeting acording to the hour
-
-let hour = time; //cambiar esto de acá/////////////////////////
-
-const greeting = document.querySelector('.greeting');
-
-function greeter(hour) {
-    console.log(hour);
-    if (5 <= hour && hour < 12) greeting.innerText = 'GOOD MORNING';
-    else if (12 <= hour && hour < 18) greeting.innerText = 'GOOD AFTERNOON';
-    else greeting.innerText = 'GOOD EVENING';
-    console.log(greeting);
+function greeter() {
+    if (5 <= hour && hour < 12) showGreeting.innerText = 'GOOD MORNING';
+    else if (12 <= hour && hour < 18) showGreeting.innerText = 'GOOD AFTERNOON';
+    else showGreeting.innerText = 'GOOD EVENING';
 }
 
-greeter(hour);
-
-//Sets day or night variable
-const main = document.querySelector('main');
-const daytimeIcon = document.querySelector('.daytime-icon');
-
-function isDayOrNight(hour) {
+//Sets day or night CSS class
+function isDayOrNight() {
     if (5 <= hour && hour < 18) {
-        daytimeIcon.classList.add('is-day');
-        daytimeIcon.classList.remove('is-night');
+        showDaytimeIcon.classList.add('is-day');
+        showDaytimeIcon.classList.remove('is-night');
         main.classList.add('is-day');
         main.classList.remove('is-night');
     } else {
-        daytimeIcon.classList.add('is-night');
-        daytimeIcon.classList.remove('is-day');
+        showDaytimeIcon.classList.add('is-night');
+        showDaytimeIcon.classList.remove('is-day');
         main.classList.add('is-night');
         main.classList.remove('is-day');
     }
 }
 
-isDayOrNight(hour);
+
