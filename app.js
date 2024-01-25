acd01d6976173ec214d4a8396f02f296fc6ecb9e
enrihg <99612570+enrihg@users.noreply.github.com>
2024-01-25 14:53:03 -0300

    more clock code refactoring

--- app.js
+++ app.js
@@ -15,119 +15,124 @@ const showGreeting = document.querySelector('.greeting');
 const showDaytimeIcon = document.querySelector('.daytime-icon');
 const showUtc = document.querySelector('.utc');
 
-let ip= 0;
-let hour= 0;
-let hours= 0;
-let minutes= 0;
-let weekNumber= 0;
-let weekDay= 0;
-let yearDay= 0;
-let unixtime=0 ;
-let timezone= 0;
-let utcOffset='';
-let city='';
-let country='';
+let ip;
+let hour;
+let hours;
+let minutes;
+let weekNumber;
+let weekDay;
+let yearDay;
+let unixtime;
+let timezone;
+let utcOffset;
+let city;
+let country;
+let date;
 
-getGeolocalizationData();
+initialLoad();
 updateHour();
-randomQuote();
 
-
-//Shows/hide the modal and quote section when clicking the less/more button
-btnLessMore.addEventListener('click', () => {
-    quote.classList.toggle('display-none');
-    modal.classList.toggle('display-none');
-    modal.classList.contains('display-none') ? btnLessMore.firstElementChild.innerText = 'MORE' : btnLessMore.firstElementChild.innerText = 'LESS';
-})
-
-
-//Gives a random quote when clicking the refresh button
-btnRefresh.addEventListener('click', randomQuote);
-
-function randomQuote() {
-    fetch('https://api.quotable.io/quotes/random')
-        .then((res) => {
-            return res.json();
-        })
-        .then((data) => {
-            quoteParagraph.innerText = `"${data[0].content}"`;
-            quoteAuthor.innerText = data[0].author;
-        })
-        .catch((e) => {
-            console.log('ERROR!!', e);
-        });
+async function initialLoad() {
+    getRandomQuote();
+    await fetchGeolocalizationData();
+    setInitialHour();
+    updateUi();
 }
 
-
-//updates the geolocalization object properties values
-function getGeolocalizationData() {
-    fetch('http://worldtimeapi.org/api/ip')
+//fetches the geolocalization data
+async function fetchGeolocalizationData() {
+    await fetch('http://worldtimeapi.org/api/ip')
         .then((res) => {
             return res.json();
         })
         .then((data) => {
             console.log(data);
-            weekNumber = data.week_number; //ver si cambiarlo
-            weekDay = data.day_of_week; //ver si cambiarlo
-            yearDay = data.day_of_year; //ver si cambiarlo
+            weekNumber = data.week_number; 
+            //the API by default shows Sunday as 0, i changed it to be 7 instead
+            weekDay = (data.day_of_week == 0) ? 7 : data.day_of_week; 
+            console.log(weekDay);
+            yearDay = data.day_of_year; 
             timezone = data.timezone;
             ip = (data.client_ip);
-            utcOffset = data.utc_offset; //ver si cambiarlo
-            unixtime = data.unixtime;
-        })
-        .then(() => {
-            fetch(`https://ipapi.co/${ip}/json`)
-                .then((res) => {
-                    return res.json();
-                })
-                .then((data) => {
-                    city = data.city;
-                    country = data.country_name;
-                })
-                .catch((e) => {
-                    console.log('IP ERROR!!', e);
-                })
+            utcOffset = data.utc_offset; 
+            unixtime = data.unixtime * 1000;
         })
         .catch((e) => {
             console.log('GEOLOCALIZATION ERROR', e);
         })
+       
+    await fetch(`https://ipapi.co/${ip}/json`)
+        .then((res) => {
+            return res.json();
+        })
+        .then((data) => {
+            console.log(data);
+            city = data.city;
+            country = data.country_name;
+        })
+        .catch((e) => {
+            console.log('IP ERROR!!', e);
+        })
 }
 
-//updates the hour every 1 second ???
+function setInitialHour() {
+    date = new Date(unixtime);
+    console.log(date);
+    hours = date.getHours();
+    minutes = date.getMinutes();
+    hour = Number(`${hours}.${minutes}`);
+    console.log(hour);
+}
+
+//updates the hour every 1 minute
 function updateHour() {
     setInterval(() => {
-        console.log(unixtime += 1);;
-        let date = new Date(unixtime * 1000); //ver cada cuanto conviene actualizar los datos
+        unixtime = unixtime + 60000;
+        date = new Date(unixtime);
+        console.log(date);
         hours = date.getHours();
         minutes = date.getMinutes();
         hour = Number(`${hours}.${minutes}`);
-        setGeolocalizationData();
-    }, 1000)
+        //checks if a new day has begun, in order to fetch new data from APIs to update the day, week day and week number
+        let isMidnight = hour == 0.0;
+        isMidnight ? initialLoad() : '' ;
+        console.log(isMidnight);
+
+        updateUi();
+    }, 60000);
 }
 
-
-//Updates the UI with geolocalization data;
-function setGeolocalizationData() {
+//updates the UI with geolocalization data from the APIs;
+function updateUi() {
     showWeekNumber.innerText = weekNumber;
     showWeekDay.innerText = weekDay;
     showYearDay.innerText = yearDay;
     showTimezone.innerText = timezone;
     showCityAndCountry.innerText = `IN ${city}, ${country}`.toUpperCase();
     showUtc.innerText = `UTC: ${utcOffset}`;
-    showTime.innerText = `${hours}:${minutes}`;
-    greeter()
+    showTime.innerText = twoDigitsHour();
+    showGreeting.innerText = greeter();
     isDayOrNight();
 }
 
-
-//Sets the greeting acording to the hour
-function greeter() {
-    if (5 <= hour && hour < 12) showGreeting.innerText = 'GOOD MORNING';
-    else if (12 <= hour && hour < 18) showGreeting.innerText = 'GOOD AFTERNOON';
-    else showGreeting.innerText = 'GOOD EVENING';
+//displays the hour in a 2 digits format, e.g: 03:05, not: 3.5
+function twoDigitsHour () {
+    let twoDigitsHour = hours < 10 ? '0'.concat(hours) : hours;
+    let twoDigitsMinutes = minutes < 10 ? '0'.concat(minutes) : minutes;
+    twoDigits = `${twoDigitsHour}:${twoDigitsMinutes}`;
+    return twoDigits;
 }
 
-//Sets day or night CSS class
+//sets the greeting acording to the hour
+function greeter() {
+    let greeting
+    if (5 <= hour && hour < 12) greeting = 'GOOD MORNING';
+    else if (12 <= hour && hour < 18) greeting = 'GOOD AFTERNOON';
+    else greeting = 'GOOD EVENING';
+    return greeting;
+}
+
+//sets day or night CSS class
 function isDayOrNight() {
     if (5 <= hour && hour < 18) {
         showDaytimeIcon.classList.add('is-day');
@@ -142,4 +147,26 @@ function isDayOrNight() {
     }
 }
 
+//shows/hide the modal and quote section when clicking the less/more button
+btnLessMore.addEventListener('click', () => {
+    quote.classList.toggle('display-none');
+    modal.classList.toggle('display-none');
+    modal.classList.contains('display-none') ? btnLessMore.firstElementChild.innerText = 'MORE' : btnLessMore.firstElementChild.innerText = 'LESS';
+})
 
+//gives a random quote when clicking the refresh button
+btnRefresh.addEventListener('click', getRandomQuote);
+
+function getRandomQuote() {
+    fetch('https://api.quotable.io/quotes/random')
+        .then((res) => {
+            return res.json();
+        })
+        .then((data) => {
+            quoteParagraph.innerText = `"${data[0].content}"`;
+            quoteAuthor.innerText = data[0].author;
+        })
+        .catch((e) => {
+            console.log('ERROR!!', e);
+        });
+}
